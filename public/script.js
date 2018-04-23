e9545e1d7b2d34441848ac843cb0628a0f9b0766
Yocheved <cheved.stern@gmail.com>
2018-04-23 12:35:40 +0300

    completed PARTNER A

--- public/script.js
+++ public/script.js
@@ -8,19 +8,35 @@ var updateCart = function () {
   $(".cart-list").empty();
   var $cartlist = $(".cart-list");
   var total = 0;
-  for(var i=0 ; i<cart.length; i++){
-    $cartlist.append("<div class=\"cart-item\">" + cart[i].name + " - $" + cart[i].price + "</div>");
+  for (var i = 0; i < cart.length; i++) {
+    $cartlist.append("<div class=\"cart-item\" data-name=\"" + cart[i].name + "\">" + cart[i].name + "(" + cart[i].amount + ") - $" +
+      cart[i].price + "<button class=\"btn remove-item\">Remove</button" + "</div>");
     total += cart[i].price;
   }
   $(".total").text(total);
 }
 
 
-var addItem = function (item) {
-  console.log(item.data()
+var addItem = function ($item) {
+  var found = false;
+  cart.find((o, i) => {
+    if (o.name === $item.data("name") && found == false) {
+      cart[i].amount++;
+      cart[i].price += $item.data("price");
+      found = true;
+    }
+  });
+  if (found == false) {
+    cart.push({ name: $item.data("name"), price: $item.data("price"), amount: 1 });
+    console.log($item.data());
+  }
 
-);
-  cart.push({name: item.data("name"), price: item.data("price")});
+}
+
+function removeItem($item) {
+  cart = $.grep(cart, function (e) {
+    return e.name != $item.data("name");
+  });
 }
 
 var clearCart = function () {
@@ -32,20 +48,18 @@ $('.view-cart').on('click', function () {
   $('.shopping-cart').show();
 });
 
-$(document).mouseup(function(e) 
-{
-    var container = $(".shopping-cart");
+$(document).mouseup(function (e) {
+  var container = $(".shopping-cart");
 
-    // if the target of the click isn't the container nor a descendant of the container
-    if (!container.is(e.target) && container.has(e.target).length === 0) 
-    {
-        container.hide();
-    }
+  // if the target of the click isn't the container nor a descendant of the container
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    container.hide();
+  }
 });
 
 $('.add-to-cart').on('click', function () {
 
-  var $item  = $(this).closest('.item');
+  var $item = $(this).closest('.item');
   addItem($item);
   updateCart();
 });
@@ -53,6 +67,13 @@ $('.add-to-cart').on('click', function () {
 $('.clear-cart').on('click', function () {
   clearCart();
 });
+
+$('.cart-list').on('click', '.remove-item', function () {
+  console.log("this = " + $(this));
+  var $item = $(this).closest('.cart-item');
+  removeItem($item);
+  updateCart();
+})
 
 // update the cart as soon as the page loads!
 updateCart();
